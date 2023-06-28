950a8980d6a94a3632ca52963b14bcfbb2480373
Ayan Bhattacharya <67321022+anefreeinity@users.noreply.github.com>
2023-06-29 00:41:03 +0530

    veriable search box and dynamic dropdown

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+
+export interface DrpDwn {
+  id: number;
+  name: string;
+  selectState: boolean;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +13,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  data: DrpDwn[] = [];
+  public isCollapsed = true;
+  prevSelectedWindow: DrpDwn | null = null;
+
   constructor() {
+    this.data = [
+      {
+        id:1,
+        name:'Home',
+        selectState: false,
+      },
+      {
+        id:2,
+        name:'Music',
+        selectState: false,
+      },
+      {
+        id:3,
+        name:'Books',
+        selectState: false,
+      }
+    ];
+
+    this.prevSelectedWindow = this.data[0];
+    this.prevSelectedWindow.selectState = true;
   }
 
-  dropDown() {
-    window.alert('ok');
+  dropDown(data: DrpDwn) {
+    if(this.prevSelectedWindow){
+      this.prevSelectedWindow.selectState = false;
+    }
+    data.selectState = true;
+    this.prevSelectedWindow = data;
+    window.alert(JSON.stringify(data));
   }
 }
+
+//ng build --output-path docs --base-href ANEFreeInIty-UI
